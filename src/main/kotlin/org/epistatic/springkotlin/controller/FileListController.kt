a847cdabcf6ff57b5eb528de4c36d73bff9a9735
SergeMerzliakov <serge.merzliakov@qantasloyalty.com>
2019-11-13 20:42:23 +1100

    Updated or added missing FOSS license header to be correct

--- src/main/kotlin/org/epistatic/springkotlin/controller/FileListController.kt
+++ src/main/kotlin/org/epistatic/springkotlin/controller/FileListController.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Serge Merzliakov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.epistatic.springkotlin.controller
 
 import com.google.common.eventbus.EventBus
@@ -13,88 +29,69 @@ import org.springframework.stereotype.Component
 import java.io.File
 
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- **/
-
-/**
  * Controller for fileList.fxml - the list if files dragged into the application
  */
 @Component
 class FileListController(eventBus: EventBus) : EventAwareController(eventBus) {
 
-	// view
-	@FXML lateinit var fileListView: ListView<File>
+   // view
+   @FXML lateinit var fileListView: ListView<File>
 
-	// model
-	val fileList = FXCollections.observableArrayList<File>()
+   // model
+   val fileList = FXCollections.observableArrayList<File>()
 
-	fun load(): Pane {
-		val loader = FXMLLoader(javaClass.getResource("/fileList.fxml"))
-		loader.setController(this)
-		return loader.load<Pane>()
-	}
+   fun load(): Pane {
+      val loader = FXMLLoader(javaClass.getResource("/fileList.fxml"))
+      loader.setController(this)
+      return loader.load<Pane>()
+   }
 
 
-	@FXML
-	fun initialize() {
-		initializePlaceHolders()
-		initializeDragAndDrop()
+   @FXML
+   fun initialize() {
+      initializePlaceHolders()
+      initializeDragAndDrop()
 
       fileListView.items = fileList.sorted()
 
       // fire event on EventBus everytime selection changes - most often Mouse Click
-		fileListView.selectionModel.selectedItemProperty().addListener { _, _, newSelection ->
-			if (newSelection != null) {
-				println("Firing FileSelectedEvent event....")
-				eventBus.post(FileSelectedEvent(newSelection))
-			}
-		}
-	}
+      fileListView.selectionModel.selectedItemProperty().addListener { _, _, newSelection ->
+         if (newSelection != null) {
+            println("Firing FileSelectedEvent event....")
+            eventBus.post(FileSelectedEvent(newSelection))
+         }
+      }
+   }
 
-	private fun initializeDragAndDrop() {
-		// accept references
-		fileListView.onDragOver = EventHandler { event ->
-			event.acceptTransferModes(TransferMode.LINK)
-			event.consume()
-		}
+   private fun initializeDragAndDrop() {
+      // accept references
+      fileListView.onDragOver = EventHandler { event ->
+         event.acceptTransferModes(TransferMode.LINK)
+         event.consume()
+      }
 
-		fileListView.onDragDropped = EventHandler { event ->
-			if (event.gestureSource == null && event.acceptedTransferMode == TransferMode.LINK) {
-				val files = event.dragboard.files
-				// we will cheat and only add the first file dragged in
-				val firstFile = files.first()
-				files.map { fileList.add(firstFile) }
-				System.out.println("Firing FileAddedEvent event....")
-				eventBus.post(FileAddedEvent(firstFile))
-				event.isDropCompleted = true
-				event.consume()
-			}
-		}
-	}
+      fileListView.onDragDropped = EventHandler { event ->
+         if (event.gestureSource == null && event.acceptedTransferMode == TransferMode.LINK) {
+            val files = event.dragboard.files
+            // we will cheat and only add the first file dragged in
+            val firstFile = files.first()
+            files.map { fileList.add(firstFile) }
+            System.out.println("Firing FileAddedEvent event....")
+            eventBus.post(FileAddedEvent(firstFile))
+            event.isDropCompleted = true
+            event.consume()
+         }
+      }
+   }
 
-	/**
-	 * Shown when lists are empty
-	 */
-	private fun initializePlaceHolders() {
-		val filePlaceHolder = Label("Drag Files Here")
-		filePlaceHolder.styleClass.add("file-placeholder")
-		fileListView.placeholder = filePlaceHolder
-	}
+   /**
+    * Shown when lists are empty
+    */
+   private fun initializePlaceHolders() {
+      val filePlaceHolder = Label("Drag Files Here")
+      filePlaceHolder.styleClass.add("file-placeholder")
+      fileListView.placeholder = filePlaceHolder
+   }
 
 
 }
--- src/main/kotlin/org/epistatic/springkotlin/controller/FilePropertiesController.kt
+++ src/main/kotlin/org/epistatic/springkotlin/controller/FilePropertiesController.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Serge Merzliakov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.epistatic.springkotlin.controller
 
 import com.google.common.eventbus.EventBus
@@ -9,54 +25,35 @@ import javafx.scene.layout.Pane
 import org.springframework.stereotype.Component
 
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- **/
-
-/**
  * Controller for fileProperties.fxml
  */
 @Component
 class FilePropertiesController(eventBus: EventBus) : EventAwareController(eventBus) {
 
-	@FXML lateinit var nameLabel: Label
-	@FXML lateinit var pathLabel: Label
-	@FXML lateinit var sizeLabel: Label
+   @FXML lateinit var nameLabel: Label
+   @FXML lateinit var pathLabel: Label
+   @FXML lateinit var sizeLabel: Label
 
-	fun load(): Pane {
-		val loader = FXMLLoader(javaClass.getResource("/fileProperties.fxml"))
-		loader.setController(this)
-		return loader.load<Pane>()
-	}
+   fun load(): Pane {
+      val loader = FXMLLoader(javaClass.getResource("/fileProperties.fxml"))
+      loader.setController(this)
+      return loader.load<Pane>()
+   }
 
-	@Subscribe
-	fun handleFileAdded(e: FileAddedEvent){
-		println("FilePropertiesController processing FileAddedEvent")
-		nameLabel.text = e.file.name
-		pathLabel.text = e.file.path
-		sizeLabel.text = e.file.length().toString()
-	}
+   @Subscribe
+   fun handleFileAdded(e: FileAddedEvent){
+      println("FilePropertiesController processing FileAddedEvent")
+      nameLabel.text = e.file.name
+      pathLabel.text = e.file.path
+      sizeLabel.text = e.file.length().toString()
+   }
 
-	@Subscribe
-	fun handleFileSelectionChanged(e: FileSelectedEvent){
-		println("FilePropertiesController processing FileSelectedEvent")
-		nameLabel.text = e.file.name
-		pathLabel.text = e.file.path
-		sizeLabel.text = e.file.length().toString()
-	}
+   @Subscribe
+   fun handleFileSelectionChanged(e: FileSelectedEvent){
+      println("FilePropertiesController processing FileSelectedEvent")
+      nameLabel.text = e.file.name
+      pathLabel.text = e.file.path
+      sizeLabel.text = e.file.length().toString()
+   }
 
 }
